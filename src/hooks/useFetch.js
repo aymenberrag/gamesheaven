f8edbb307fa43d2e13b56c8e372f481e85af1c9a
aymenberrag <aymenberrag2017@gmail.com>
2022-11-02 09:58:29 +0000

    fix the bugs

--- src/hooks/useFetch.js
+++ src/hooks/useFetch.js
@@ -1,10 +1,8 @@
 import { useState,useEffect } from "react"
-import { useParams} from "react-router-dom"
-const useFetch=(params=null)=>{
+const useFetch=(page,params=null)=>{
     const [isLoading,setIsLoading]=useState(true)
     const [err,setErr]=useState(null)
     const [data,setData]=useState(null)
-    const {page}=useParams()
     let q=""
     if(params){
         for(let param of params.keys()){
@@ -13,7 +11,6 @@ const useFetch=(params=null)=>{
     }
     useEffect(()=>{
         setIsLoading(true)
-        
         fetch(`https://api.rawg.io/api/${page}?key=${"fd7cfdf1daa045669aae0980fa558939"}${q}`)
         .then(res=>{
             if(res.ok){
